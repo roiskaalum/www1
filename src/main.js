9f4bc5981827e4aa1b4180d7682c22db73ea7a08
roiskaalum <roi.skaalum@gmail.com>
2025-03-17 18:55:04 +0100

    removed forEach error, apparently it didn't even need to be there.

--- src/main.js
+++ src/main.js
@@ -92,13 +92,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   //Hamburger Click Function:
   const burgerMenu = document.querySelector("#burger-menu-button");
-  const navList = document.getElementsByClassName("nav-list");
   const navContainer = document.querySelector(".nav-container");
   //TODO: Swap src image for hamburger for an opened version of the same color variant.
   burgerMenu.addEventListener("click", () => {
     navContainer.classList.toggle("active");
     burgerMenu.classList.toggle("active");
-    navList.toggle("active");
   });
   display("src/orders.json");
 });
