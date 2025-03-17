36e4eda35ed048cccb3e36ae51f3f06183ad6e21
roiskaalum <roi.skaalum@gmail.com>
2025-03-17 17:58:15 +0100

    version 1.0 of Star Pizza with JSON, darkmode and hamburger menu

--- v4_StarPizza_with_js/src/main.js
+++ v4_StarPizza_with_js/src/main.js
@@ -1,12 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const burgerMenu = document.querySelector('.burger-menu');
-//     const navList = document.querySelector('header navbar ul');
-  
-//     burgerMenu.addEventListener('click', () => {
-//       burgerMenu.classList.toggle('active');
-//       navList.classList.toggle('active');
-//     });
-//   });
 display("./src/orders.json");
 async function display(file)
 {
@@ -16,12 +7,12 @@ async function display(file)
   let html = document.getElementsByClassName("container-orders")[0];
   let pizzaElement = "";
   t.pizzas.forEach(item => {
-    console.log(item.name + " : " + item.ingredients);
     let ingredients = "";
-    for(let i = 0; i < ingredients.length; i++)
+    console.log(item.ingredients.length)
+    for(let i = 0; i < item.ingredients.length; i++)
     {
-      ingredients += ingredients[i];
-      if(i != ingredients.length-1)
+      ingredients += item.ingredients[i];
+      if(i < item.ingredients.length-1)
       {
         ingredients += ", ";
       }
@@ -33,26 +24,80 @@ async function display(file)
     pizzaElement += `
     <div class="container-orders-item">
       <h4 class="container-orders-item-title">${item.name}</h2>
-      <a href="${item.image}_big.jpg"><img class="container-orders-img" src="${item.image}_small.jpg"></a>
+      <a href="${item.image}_big.jpg"><img class="container-orders-img" src="${item.image}_small.jpg" alt="${item.name} Thumbnail"></a>
       <p class="container-orders-p">${ingredients}</p>
     </div>
     `
   });
 
   html.innerHTML = pizzaElement;
-
+  let drinksElement = "";
   t.drinks.forEach(item => {
-    console.log(item);
-  })
+    let options = "";
+    for(let i = 0;i < item.options.length; i++)
+    {
+      options += item.options[i];
+      if(i <= item.options.length-1)
+      {
+        options += ", ";
+      }
+      else
+      {
+        options = ".";
+      }
+    }
+    drinksElement += `
+    <div class="container-orders-item">
+      <h4 class="container-orders-title">${item.name}</h4>
+      <a href="${item.image}_big.jpg"><img class="container-orders-img" src="${item.image}_small.jpg"></a>
+      <p class="container-orders-p">${options}</p>
+    </div>
+    `
+  });
+  html.innerHTML += drinksElement;
 }
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.querySelector("#theme-toggle");
+  const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const storedTheme = localStorage.getItem("theme");
+  const themeIcon = document.querySelector("#theme-icon");
+  const burgerIcon = document.querySelector("#burgerImage");
 
+  //Darkmode detection function:
+  if (storedTheme == "dark" || (!storedTheme && prefersDark)) {
+    document.body.classList.add("dark");
+    themeIcon.src = "/src/images/darkmode_white.png";
+    burgerIcon.src = "/src/images/nav-burger-menu-white.png";
+  }
 
-// async function display(file)
-// {
-//   fetch(file)
-//   .then(o => o.text())
-//   .then(t => console.log(t));
-// }
+  //Darkmode Click Function:
+  themeToggle.addEventListener("click", () => {
+      document.body.classList.toggle("dark");
+      localStorage.setItem("theme", document.body.classList.contains("dark") ? "dark" : "light");
+      if(themeIcon.src.includes("darkmode_white.png"))
+      {
+        themeIcon.src = "/src/images/darkmode_black.png";
+        burgerIcon.src = "/src/images/nav-burger-menu.png";
+      }
+      else
+      {
+        themeIcon.src = "/src/images/darkmode_white.png";
+        burgerIcon.src = "/src/images/nav-burger-menu-white.png";
+      }
+    });
 
+    //Hamburger Click Function:
+    const burgerMenu = document.querySelector("#burger-menu-button");
+    const navList = document.querySelector(".nav-list");
+    const navContainer = document.querySelector(".nav-container");
+    //TODO: Swap src image for hamburger for an opened version of the same color variant.
+    burgerMenu.addEventListener("click", () => {
+        navContainer.classList.toggle("active");
+        burgerMenu.classList.toggle("active");
+        navList.forEach(item => {
+          item.classList.toggle("active");
+        });
+    });
+});
